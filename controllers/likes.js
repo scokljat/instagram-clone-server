4bedd319ab7bb4a9cd2339c33a7e82d0dcdd6969
scokljat <selma.cokljat@hotmail.com>
2022-05-25 13:30:21 +0200

    Adds dislike post to likes controller

--- controllers/likes.js
+++ controllers/likes.js
@@ -15,12 +15,29 @@ const likePost = async (req, res) => {
     },
   });
 
-  res.json(liked);
+  const post = await prisma.post.findUnique({
+    where: { id: values.postId },
+    include: { user: true, likes: true },
+  });
+  res.status(200).send(post);
 };
 
 const getLikes = async (req, res) => {
   const likes = await prisma.likes.findMany();
-  res.json(likes);
+  res.status(200).send(likes);
 };
 
-module.exports = { likePost, getLikes };
+const dislikePost = async (req, res) => {
+  const values = req.body;
+  const disliked = await prisma.likes.deleteMany({
+    where: { postId: req.body.postId, userId: req.body.userId },
+  });
+
+  const post = await prisma.post.findUnique({
+    where: { id: values.postId },
+    include: { user: true, likes: true },
+  });
+  res.status(200).send(post);
+};
+
+module.exports = { likePost, getLikes, dislikePost };
