adccafbe63c206d119eb7b5c20f318ed243fd40a
scokljat <selma.cokljat@hotmail.com>
2022-05-25 13:32:00 +0200

    Update likes routes

--- routes/likes.js
+++ routes/likes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { likePost, getLikes } = require("../controllers/likes");
+const { likePost, getLikes, dislikePost } = require("../controllers/likes");
 
 const router = express.Router();
 
 router.get("/likes", getLikes);
 
 router.post("/likes", likePost);
+
+router.post("/dislikes", dislikePost);
 
 module.exports = router;
